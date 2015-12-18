f62b58dd9a4beb3b42e24901c9e870403357ab66
Selen ADALI <selenadali94@gmail.com>
2015-12-18 21:01:15 +0200

    First Commit
    Create Project

---
+++ damacana_web-api/damacana_web-api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace damacana_web_api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
